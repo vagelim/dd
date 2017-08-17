afc874951a3989fdb0339665bfbf16cfead9fac6
Evan Mouzakitis <vagelim@users.noreply.github.com>
2017-08-17 14:07:07 -0400

    simpler setup for simpler setups
    also add a couple of function aliases

--- api.py
+++ api.py
@@ -5,21 +5,34 @@ import json
 import os
 import sys
 
-def main(case=None):
-    SETTINGS = os.environ['GET_SCREEN_CREDS']
-    keys = json.load(open(SETTINGS, 'r'))
 
-    if case is None:
-        case = "prod"
-    try:
-        options = {
-            'api_key': keys[case]['API'],
-            'app_key': keys[case]['APP']
-        }
-    except KeyError:
-        case = "prod"
+def main(case=None):
+
+    options = {'api_key': '', 'app_key': ''}
+    # Get the settings from a settings file
+    if os.environ.get('GET_SCREEN_CREDS', None):
+        keys = json.load(open(os.environ['GET_SCREEN_CREDS'], 'r'))
+
+        if case is None:
+            case = "prod"
+        try:
+            options = {
+                'api_key': keys[case]['API'],
+                'app_key': keys[case]['APP']
+            }
+        except KeyError:
+            case = "prod"
+
+    elif os.environ.get('DD_API', None):
+        case = "default"
+        options['api_key'] = os.environ['DD_API']
+        options['app_key'] = os.environ['DD_APP']
+
     initialize(**options)
-    
+
+    event = api.Event.create
+    post = api.Metric.send
+    search = api.Infrastructure.search
     TAGS = api.Tag.get_all()['tags']
     HOSTS = api.Infrastructure.search(q='hosts:')['results']['hosts']
     print "====================="
@@ -28,7 +41,6 @@ def main(case=None):
     print locals().keys()
     print "=========================================="
     code.interact(local=dict(globals(), **locals()))
-
 
 
 if __name__ == "__main__":
